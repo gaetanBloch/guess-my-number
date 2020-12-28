74cc3a6187d60ca9ac3d572866254e33ac525f1a
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-12-28 14:47:58 +0100

    Retrive all useful elements

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const message = document.querySelector('.message');
+const myNumber = document.querySelector('.number');
+const guessNumber = document.querySelector('.guess');
+const score = document.querySelector('.score');
+const highScore = document.querySelector('.highScore');
+
+const checkButton = document.querySelector('.check');
+const againButton = document.querySelector('.again');
