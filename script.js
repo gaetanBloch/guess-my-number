cfa8b11ca4818d800f647e40c7f2f23431547dab
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-12-29 05:22:07 +0100

    First implementation of the game

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
 'use strict';
 
 const message = document.querySelector('.message');
-const myNumber = document.querySelector('.number');
+const myNumberText = document.querySelector('.number');
 const guessNumber = document.querySelector('.guess');
 const score = document.querySelector('.score');
-const highScore = document.querySelector('.highScore');
+const highScoreText = document.querySelector('.highScore');
 
 const checkButton = document.querySelector('.check');
 const againButton = document.querySelector('.again');
+
+const MAX_NUMBER = 20;
+
+const checkHighScore = newScore => {
+  newScore = Number(newScore);
+  const highScore = Number(highScoreText.textContent);
+  console.log(newScore, highScore);
+  if (newScore > highScore) {
+    highScoreText.textContent = newScore;
+  }
+};
+
+const play = myNumber => {
+  checkButton.addEventListener('click', () => {
+    const guess = guessNumber.value;
+    if (guess > myNumber) {
+      message.textContent = 'Too High...';
+      score.textContent -= 1;
+    } else if (guess < myNumber) {
+      message.textContent = 'Too Low...';
+      score.textContent -= 1;
+    } else {
+      message.textContent = 'Correct Number!';
+      myNumberText.textContent = myNumber;
+      checkHighScore(guess);
+    }
+  });
+};
+
+const randomNumber = () => Math.trunc(Math.random() * MAX_NUMBER + 1);
+
+play(randomNumber());
+
+againButton.addEventListener('click', () => {
+  message.textContent = 'Start Guessing...';
+  score.textContent = 20;
+  myNumberText.textContent = '?';
+  guessNumber.value = '';
+  play(randomNumber());
+});
